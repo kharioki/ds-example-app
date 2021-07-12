78cc9da455b0bfe6a3f4e38795d353242ced0486
tony <antolisah@gmail.com>
2021-07-13 02:09:13 +0300

    reference my published design system

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,12 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  refs: {
+    'design-system': {
+      title: 'My design system',
+      //👇 The url provided by Chromatic when it was deployed
+      url: 'https://60e8287561fef20049e02f25-tookjytzdx.chromatic.com',
+    },
+  },
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
